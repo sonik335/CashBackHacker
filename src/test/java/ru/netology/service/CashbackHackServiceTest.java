0b48546582f158357a80ca929e33d17fe41736e4
Kirill <sonik3000.83@gmail.com>
2022-09-28 13:49:34 +0300

    initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 public class CashbackHackServiceTest {
 
 
-
     @Test
     public void testRemain() {
 
@@ -15,33 +14,30 @@ public class CashbackHackServiceTest {
         int expected = 1000;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual,expected);
-
+        Assert.assertEquals(actual, expected);
 
 
     }
 
     @Test
-    public void testRemainEquals(){
+    public void testRemainEquals() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void testRemainLess(){
+    public void testRemainLess() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int expected = 100;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
     }
-
-
 
 
 }
